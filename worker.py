e87d6a6302799e4c6ce18e722b9499da723891f0
Korzhak <navchalkin@ex.ua>
2020-05-06 17:05:22 +0300

    added comment to worker.py

--- worker.py
+++ worker.py
@@ -1,10 +1,11 @@
-from arduino_manager import ArduinoManager
+"""Main file for ArduinoFirmataManager
+:author: B. Korzhak
+:date: 06.05.2020
+"""
 from views import FirmataView
 
 
+# Main flow for ArduinoFirmataManager
 if __name__ == "__main__":
     view = FirmataView()
     view.window.mainloop()
-    # am = ArduinoManager()
-    # for i in range(10):
-    #     am.blink()
